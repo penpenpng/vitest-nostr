57a11421cb497ca69cb87b646c4c49caeb763e51
penpenpng <poman1638m@gmail.com>
2023-06-25 22:51:48 +0900

    init

---
+++ src/__test__/index.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest";
+
+test("Hello vitest", () => {
+  expect(1).toBe(1);
+});
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+  build: {
+    lib: {
+      name: "vitest-nostr-relay-mock",
+      fileName: (format) => `index.${format}.js`,
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs", "umd"],
+    },
+    sourcemap: true,
+  },
+});
